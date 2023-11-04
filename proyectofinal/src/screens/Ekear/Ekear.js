c444a380c7ff1f3092199a95e9a08fba6a8b68f2
katerinacejas <katerinacejas3@gmail.com>
2023-11-04 19:20:35 -0300

    ekearrrrr

--- proyectofinal/src/screens/Ekear/Ekear.js
+++ proyectofinal/src/screens/Ekear/Ekear.js
@@ -1,10 +1,25 @@
 import styles from './Ekear.style'
 import React, { useState } from 'react';
 import { View, TextInput, Image, Text, TouchableOpacity } from 'react-native';
-
+import { useGetCancionesQuery } from '../../services/cancionesApi';
 
 const Ekear = () => {
-    const [eko, setEko] = useState('');
+    const [eko, setEko] = useState({
+        texto: '',
+        infoDeLaCancion: {
+            cancion: '',
+            portada: '',
+            autor: '',
+        },
+        momento: '',
+        usuario: '',
+    });
+
+    const { data, isLoading, error } = useGetCancionesQuery();
+    
+    const seleccionarCancion = () => {
+       return //navegar a la pantalla seleccionar cancion
+    }
 
     return (
         <View style={styles.container}>
@@ -14,17 +29,33 @@ const Ekear = () => {
                     style={styles.imagenDePerfil}
                 />
                 <View style={styles.ekoContenido}>
-                    <Text style={styles.nombreUnico}>TuNombreDeUsuario</Text>
-                    <TextInput
+                    <Text style={styles.nombre}>TuNombreDeUsuario</Text>
+                    <Text style={styles.nombreUnico}>@nombreUnico</Text>
+                </View>
+            </View>
+            <TextInput
                         style={styles.input}
                         placeholder="¿Qué estas escuchando?"
                         multiline
-                        value={eko}
-                        onChangeText={(text) => setEko(text)}
+                        value={eko.texto}
+                        maxLength={256}
+                        onChangeText={(texto) => setEko(texto)}
                     />
-                </View>
-            </View>
+            <TouchableOpacity style={styles.botonSeleccionarCancion}>
+                <Text
+                    style={styles.textoBoton}
+                    onPress={seleccionarCancion}>
+                    Seleccionar Canción
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.botonEkear}>
+                <Text
+                    style={styles.textoBoton}
+                    onPress={seleccionarCancion}>
+                    Ekear!
+                </Text>
+            </TouchableOpacity>
         </View>
-    );
+    )
 }
 export default Ekear
